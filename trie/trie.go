3317f92044fa771c3c2cd083e22ab5a61c9f2bab
HsiaoCz <984274788@qq.com>
2023-05-31 22:19:38 +0800

    router

--- trie/trie.go
+++ trie/trie.go
@@ -1,5 +1,7 @@
 package trie
 
+import "strings"
+
 // Router维护一个根节点
 type Router struct {
 	root map[string]*node
@@ -11,7 +13,25 @@ type Router struct {
 // 就是将/user/login等字符串进行切割，分块保存到前缀树上
 
 func (r *Router) AddRouter(pattern string, danode string) {
+	if r.root == nil {
+		r.root = make(map[string]*node)
+	}
+	root, ok := r.root["/"]
+	// 创建根路由
+	if !ok {
+		root = &node{
+			part: "/",
+		}
+		r.root["/"] = root
+	}
 
+	parts := strings.Split(strings.Trim(pattern, "/"), "/")
+	for _, part := range parts {
+		if part == "" {
+			panic("pattern不符合格式...")
+		}
+		root.addNode(part)
+	}
 }
 
 func (r *Router) GetRouter(pattern string) *node {
@@ -31,7 +51,7 @@ type node struct {
 // 1.注册节点:新建一个node节点
 // 2.查找节点
 
-func (n *node) addNode(part string, data string) *node {
+func (n *node) addNode(part string) *node {
 	return nil
 }
 
