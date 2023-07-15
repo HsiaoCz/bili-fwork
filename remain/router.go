1e22247ff2048d7ba819b238067a7882fa390851
HsiaoCz <984274788@qq.com>
2023-07-15 14:24:04 +0800

    add server but remain

--- remain/router.go
+++ remain/router.go
@@ -1,1 +1,73 @@
 package remain
+
+import "strings"
+
+// 路由树
+/**
+
+{
+	"GET":node{},
+	"POST":node{},
+	"DELETE":node{},
+	.....
+}
+
+**/
+// 每个请求方法维护一个路由树
+type router struct {
+	tress map[string]*node
+}
+
+func newRouter() *router {
+	return &router{tress: map[string]*node{}}
+}
+
+// 树中的一个节点
+type node struct {
+	part string
+	// children 静态路由
+	children map[string]*node
+	// handleFunc 存当前节点上的视图函数
+	handleFunc HandleFunc
+	// 参数路由
+	// 一个位置只能有一个参数路由
+	// 并且静态路由的优先级大于动态路由
+	paramChildern *node
+}
+
+func (n *node) addNode(part string) *node {
+
+	if strings.HasPrefix(part, ":") && n.paramChildern == nil {
+		n.paramChildern = &node{part: part}
+		return n.paramChildern
+	}
+
+	// 判断当前节点有没有children属性，就是是不是nil
+	if n.children == nil {
+		n.children = make(map[string]*node)
+	}
+	child, ok := n.children[part]
+	if !ok {
+		child = &node{
+			part: part,
+		}
+		n.children[part] = child
+	}
+	return child
+}
+
+func (n *node) getNode(part string) *node {
+	if n.children == nil {
+		return nil
+	}
+
+	// 正常思路，先到静态路由中找
+	child, ok := n.children[part]
+	if !ok {
+		if n.paramChildern != nil {
+			return n.paramChildern
+		}
+		return nil
+	}
+	return child
+}
--- remain/server.go
+++ remain/server.go
@@ -1,6 +1,15 @@
 package remain
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 type server interface {
 	// 这要求我们的engine实现多路复用功能
@@ -11,12 +20,66 @@ type server interface {
 	Stop() error
 }
 
-type HTTPServer struct{}
+// 这里定义一个视图函数
+type HandleFunc func(w http.ResponseWriter, r *http.Request)
+
+// 这里用选项模式添加一个优雅关机的功能
+
+type HTTPOption func(h *HTTPServer)
+
+func WithHTTPServerStop(fn func() error) HTTPOption {
+	return func(h *HTTPServer) {
+		if fn == nil {
+			fn = func() error {
+				fmt.Println("11111111")
+				quit := make(chan os.Signal)
+				// 如果匹配到中断信息，会将信号传到channel里面
+				// 如果没有匹配到，channel会一直阻塞
+				signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+				<-quit
+				log.Println("shutdown Server ....")
+
+				// 创建一个超时的上下文，在这里等待5秒钟
+				// 等待任务执行完毕
+				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+				defer cancel()
+
+				if err := h.srv.Shutdown(ctx); err != nil {
+					log.Fatal("server shutdown", err)
+				}
+
+				// 关闭之后执行的操作
+				select {
+				case <-ctx.Done():
+					log.Println("timeout of 5 seconds...")
+				}
+				return nil
+			}
+		}
+		h.stop = fn
+	}
+}
+
+type HTTPServer struct {
+	srv  *http.Server
+	stop func() error
+}
 
 func (h *HTTPServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
-func (h *HTTPServer) Start(addr string) error                          { return nil }
-func (h *HTTPServer) Stop() error                                      { return nil }
+func (h *HTTPServer) Start(addr string) error {
+	h.srv = &http.Server{
+		Addr:    addr,
+		Handler: h,
+	}
+	log.Println("the server is runing on port", addr)
+	return h.srv.ListenAndServe()
+}
+func (h *HTTPServer) Stop() error { return h.stop() }
 
-func NewHTTPServer() *HTTPServer {
-	return &HTTPServer{}
+func NewHTTPServer(opts ...HTTPOption) *HTTPServer {
+	h := &HTTPServer{}
+	for _, opt := range opts {
+		opt(h)
+	}
+	return h
 }
